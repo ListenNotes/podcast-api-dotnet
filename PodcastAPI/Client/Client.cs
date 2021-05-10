d35e1a51e6cbe061bbbba7ada00f1a502aa6d3b6
Joe Moceri <jmoceri34@gmail.com>
2021-05-10 14:23:21 -0700

    changes

---
+++ PodcastAPI/Client/Client.cs
@@ -0,0 +1,52 @@
+﻿using PodcastAPI.Client;
+using RestSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ListenNotes.PodcastApiClient
+{
+    public sealed class Client
+    {
+        private readonly string baseUrlTest = "https://listen-api-test.listennotes.com/api/v2";
+        private readonly string baseUrlProd = "https://listen-api.listennotes.com/api/v2";
+        private readonly string userAgent;
+        private readonly IRestClient restClient;
+
+        public Client()
+        {
+            var version = GetType().Assembly.GetName().Version.ToString();
+
+            userAgent = $"podcasts-api-dotnet {version}";
+
+            restClient = new RestClient(baseUrlTest);
+
+            restClient.AddDefaultHeader("User-Agent", userAgent);
+        }
+
+        public Client(string apiKey)
+        {
+            var version = GetType().Assembly.GetName().Version.ToString();
+
+            this.userAgent = $"podcasts-api-dotnet {version}";
+
+            this.restClient = new RestClient(baseUrlProd);
+
+            restClient.AddDefaultHeader("X-ListenAPI-Key", apiKey);
+
+            restClient.AddDefaultHeader("User-Agent", userAgent);
+        }
+
+        public async Task<ApiResponse> Search(IDictionary<string, string> parameters)
+        {
+            var request = new RestRequest("search", Method.GET);
+
+            request.AddJsonBody(parameters);
+
+            var response = await restClient.ExecuteAsync(request);
+
+            var result = new ApiResponse(response.Content, response);
+
+            return result;
+        }
+    }
+}
---
+++ PodcastAPI/Sample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListenNotes.PodcastApiClient
+{
+    public class Sample
+    {
+        public static void Main()
+        {
+            try
+            {
+                var client = new Client();
+                var parameters = new Dictionary<string, string>();
+
+                parameters.Add("q", "Test");
+
+                var result = client.Search(parameters).Result;
+
+                var jsonObject = result.ToJSON<object>();
+
+                var freeQuota = result.GetFreeQuota();
+
+                var usage = result.GetUsage();
+
+                var nextBillingDate = result.GetNextBillingDate();
+
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.Read(); 
+        }
+    }
+}
